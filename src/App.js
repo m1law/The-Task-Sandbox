e7175f0dbdc20b8f75241a0f172459bcdc33802d
Eddy Lai <edwardjlai@gmail.com>
2025-03-11 13:00:26 -0700

    hello world start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:5000/api/items')
-            .then(response => response.json())
-            .then(data => setItems(data));
-    }, []);
 
     return (
         <div className="App">
-            <h1>Items from Node.js API</h1>
-            <ul>
-                {items.map(item => (
-                    <li key={item.id}>{item.name}</li>
-                ))}
-            </ul>
+            <h1>Hello World</h1>
         </div>
     );
 }
