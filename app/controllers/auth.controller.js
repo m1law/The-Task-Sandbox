e1388aa8f9a00bfbb22de3d0b133fffcee93983e
Eddy Lai <edwardjlai@gmail.com>
2025-03-24 16:41:56 -0700

    remove roles from login and signup

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -16,24 +16,7 @@ export function signup(req, res) {
     password: bcrypt.hashSync(req.body.password, 8)
   })
     .then(user => {
-      if (req.body.roles) {
-        Role.findAll({
-          where: {
-            name: {
-              [Op.or]: req.body.roles
-            }
-          }
-        }).then(roles => {
-          user.setRoles(roles).then(() => {
-            res.send({ message: "User was registered successfully!" });
-          });
-        });
-      } else {
-        // user role = 1
-        user.setRoles([1]).then(() => {
-          res.send({ message: "User was registered successfully!" });
-        });
-      }
+      res.send({ message: "User was registered successfully!" });
     })
     .catch(err => {
       res.status(500).send({ message: err.message });
@@ -71,18 +54,12 @@ export function signin(req, res) {
                                 expiresIn: 86400, // 24 hours
                               });
 
-      var authorities = [];
-      user.getRoles().then(roles => {
-        for (let i = 0; i < roles.length; i++) {
-          authorities.push("ROLE_" + roles[i].name.toUpperCase());
-        }
-        res.status(200).send({
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          roles: authorities,
-          accessToken: token
-        });
+      res.status(200).send({
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        roles: [],
+        accessToken: token
       });
     })
     .catch(err => {
