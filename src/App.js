c36712c488f3a8d20cfe3caa56e6e0732e815d77
Michael <l4wmich43l@gmail.com>
2025-03-28 16:32:17 -0700

    changed the logo real quick

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const App = () => {
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <Link to={"/"} className="navbar-brand">
-          bezKoder
+          The Task Sandbox
         </Link>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
